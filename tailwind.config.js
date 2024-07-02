4134d090b7b610d7dc56507e5fce37fbd0322f31
Aron <blazte.aster@gmail.com>
2024-07-02 13:41:31 -0500

    config font in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
     content: ['./src/**/*.{html,ts}'],
     theme: {
         extend: {},
+        fontFamily: {
+            headingline: ['Bebas Neue', 'sans-serif'],
+            title: ['Cabin', 'sans-serif'],
+            body: ['Nunito', 'sans-serif'],
+        },
     },
     plugins: [],
 }
