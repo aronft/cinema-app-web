692c7cff80d94f4e6287bf3b55718b641ea5e136
Aron <blazte.aster@gmail.com>
2024-07-10 10:00:57 -0500

    Header component (#6)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () =>
+            import(
+                './components/layouts/public-layout/public-layout.component'
+            ).then((m) => m.PublicLayoutComponent),
+    },
+    // {
+    //     path: '/movies',
+    // },
+    // {
+    //     path: '/theaters',
+    // },
+]
